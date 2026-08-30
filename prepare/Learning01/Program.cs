71339319de22e2170b3091caa409fa179a8c5b5f
Atukunda-Jacob <atukundajacob42@gmail.com>
2026-08-30 15:31:42 +0300

    Add Program.cs to prompt for favorite color

---
+++ prepare/Learning01/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}.");
+    }
+}
