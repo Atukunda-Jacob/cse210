cf15daca11e7f5f9a6391967ad6353892c8f6e7b
Atukunda-Jacob <atukundajacob42@gmail.com>
2026-09-14 13:37:48 +0300

    Add Word class for scripture memorization

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,43 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            string hidden = "";
+            foreach (char c in _text)
+            {
+                hidden += "_";
+            }
+            return hidden;
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
