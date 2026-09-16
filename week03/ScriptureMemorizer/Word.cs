fae6944705d1425cca7c69da6d089bd9b97eeee4
Atukunda-Jacob <atukundajacob42@gmail.com>
2026-09-16 09:36:56 +0300

    Refactor Hide and IsHidden methods in Word class

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -8,31 +8,14 @@ public class Word
         _text = text;
         _isHidden = false;
     }
-
-    public void Hide()
-    {
-        _isHidden = true;
-    }
-
-    public void Show()
-    {
-        _isHidden = false;
-    }
-
-    public bool IsHidden()
-    {
-        return _isHidden;
-    }
-
+    public void Hide() { _isHidden = true; }
+    public bool IsHidden() { return _isHidden; }
     public string GetDisplayText()
     {
         if (_isHidden)
         {
             return new string('_', _text.Length);
         }
-        else
-        {
-            return _text;
-        }
+        return _text;
     }
 }
