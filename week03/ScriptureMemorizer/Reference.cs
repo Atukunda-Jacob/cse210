85fad7c6d1275be984ea8096f9fbf0863bee5086
Atukunda-Jacob <atukundajacob42@gmail.com>
2026-09-16 09:53:51 +0300

    Refactor Reference class constructors and GetDisplayText

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -4,13 +4,32 @@ public class Reference
     private int _chapter;
     private int _verse;
     private int _endVerse;
+
     public Reference(string book, int chapter, int verse)
-    { _book=book; _chapter=chapter; _verse=verse; _endVerse=0; }
-    public Reference(string book, int chapter, int start, int end)
-    { _book=book; _chapter=chapter; _verse=start; _endVerse=end; }
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = 0;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
     public string GetDisplayText()
     {
-        if (_endVerse == 0) return $"{_book} {_chapter}:{_verse}";
-        return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        if (_endVerse == 0)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
     }
 }
