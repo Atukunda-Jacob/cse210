c460c6abaeab4b07985f3d76a96bde8f85452d74
Atukunda-Jacob <atukundajacob42@gmail.com>
2026-09-16 09:43:31 +0300

    Refactor Scripture class into Reference class

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,31 +1,16 @@
-public class Scripture
+public class Reference
 {
-    private Reference _reference;
-    private List<Word> _words;
-    public Scripture(Reference reference, string text)
-    {
-        _reference = reference;
-        _words = text.Split(' ').Select(w => new Word(w)).ToList();
-    }
-    public void HideRandomWords(int numberToHide)
-    {
-        Random rand = new Random();
-        var visible = _words.Where(w =>!w.IsHidden()).ToList();
-        for(int i=0; i<numberToHide && visible.Count>0; i++)
-        {
-            int index = rand.Next(visible.Count);
-            visible[index].Hide();
-            visible.RemoveAt(index);
-        }
-    }
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+    public Reference(string book, int chapter, int verse)
+    { _book=book; _chapter=chapter; _verse=verse; _endVerse=0; }
+    public Reference(string book, int chapter, int start, int end)
+    { _book=book; _chapter=chapter; _verse=start; _endVerse=end; }
     public string GetDisplayText()
     {
-        string refText = _reference.GetDisplayText();
-        string scriptureText = string.Join(" ", _words.Select(w => w.GetDisplayText()));
-        return $"{refText} {scriptureText}";
-    }
-    public bool IsCompletelyHidden()
-    {
-        return _words.All(w => w.IsHidden());
+        if (_endVerse == 0) return $"{_book} {_chapter}:{_verse}";
+        return $"{_book} {_chapter}:{_verse}-{_endVerse}";
     }
 }
