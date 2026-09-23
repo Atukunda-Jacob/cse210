bf5f0295a3ee3b170c4f75437ada7e296d3aba92
Atukunda-Jacob <atukundajacob42@gmail.com>
2026-09-23 23:59:42 +0300

    Implement Order class for managing customer orders

---
+++ week4/OnlineOrdering/Order.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Text;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalPrice()
+    {
+        double total = 0;
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+        total += _customer.IsInUSA() ? 5 : 35;
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        StringBuilder label = new StringBuilder();
+        label.AppendLine("Packing Label:");
+        foreach (Product product in _products)
+        {
+            label.AppendLine($"{product.GetName()} - {product.GetProductId()}");
+        }
+        return label.ToString();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress()}";
+    }
+} 
